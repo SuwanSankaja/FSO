1353228964c30750837d267626a47b3ebd901e11
Suwan-Aiken <suwan.sankaja@gmail.com>
2024-10-20 19:01:22 +0530

    2.5 completed

--- part2/course_information/src/App.jsx
+++ part2/course_information/src/App.jsx
@@ -1,3 +1,5 @@
+import Course from "./components/Course"
+
 const App = () => {
   const courses = [
     {
@@ -55,62 +57,3 @@ const App = () => {
 
 export default App
 
-const Course = (props) => {
-  console.log(props.course.name);
-  // console.log(props)
-  const headername =props.course.name;
-  const parts = props.course.parts;
-
-  return (
-    <div>
-    <Header header={headername} />
-    <Content parts={parts} />
-    </div>
-  );
-}
-
-const Header = (props) => {
-  // console.log(props.header);
-  return (
-    <h2>{props.header}</h2>
-  )
-}
-
-const Content = (props) => {
-  // console.log(props.parts);
-  // console.log(props.parts[0].id)
-  return(
-    <>
-    {/* <Part name={props.parts[0].name} exercise= {props.parts[0].exercises} />
-    <Part name={props.parts[1].name} exercise= {props.parts[1].exercises} />
-    <Part name={props.parts[2].name} exercise= {props.parts[2].exercises} /> */}
-
-    {props.parts.map((part) => (
-            <Part key={part.id} name={part.name} exercise={part.exercises} />
-          ))}
-    <Total parts={props.parts}/>
-    </>
-  )
-}
-
-const Part = (props) => {
-  // console.log(props.name,props.exercise);
-  return(
-    <p> {props.name} {props.exercise} </p>
-  )
- 
-}
-
-const Total = (props) => {
-  // console.log(props.parts[0].exercises);
-  // console.log(props[0])
-  // (props.parts.map(part) => (part.exercises))
-  const exercisesArray = props.parts.map((part) => part.exercises);
-  // console.log(exercisesArray);
-
-  const total = exercisesArray.reduce((s,p) => s+p);
-  // console.log(total);
-  return(
-    <p><b> total of {total} exercises </b> </p>
-  )
-}
